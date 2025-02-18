7450ca32906e6be4a2cdf1433ec906c01e53b14f
Soders <contact.soders@protonmail.com>
2025-02-18 14:37:22 -0500

    02/18/25  02:37:22 PM

---
+++ bb-tracker.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+NC='\033[0m'
+
+mkdir /tmp/bb-tracker/ 2> /dev/null
+mkdir /tmp/bb-tracker/calls 2> /dev/null
+mkdir /tmp/bb-tracker/json 2> /dev/null
+mkdir ~/.config/bb-tracker/ 2> /dev/null
+touch ~/.config/bb-tracker/api-key.txt
+touch ~/.config/bb-tracker/username.txt
+touch ~/.config/bb-tracker/account-id.txt
+touch /tmp/bb-tracker/log.txt
+log_timestamp=$(date "+%D  %I:%M:%S %p")
+api_key=$(cat ~/.config/bb-tracker/api-key.txt)
+
+api_key_setup () {
+unset bb_tracker_api_key
+prompt="Enter BB apiKey: "
+while IFS= read -p "$prompt" -r -s -n 1 char
+do
+    if [[ $char == $'\0' ]]
+    then
+        break
+    fi
+    prompt='*'
+    bb_tracker_api_key+="$char"
+done
+echo
+echo "$bb_tracker_api_key" > ~/.config/bb-tracker/api-key.txt
+read -p "$(echo -e ${RED}"Enter account ID: "${NC})" ACCOUNT_ID
+echo "$ACCOUNT_ID" > ~/.config/bb-tracker/account-id.txt
+
+sed -i "s/replace-id/$ACCOUNT_ID/g" /tmp/bb-tracker/calls/bb-tracker-login.sh
+echo -e "${GREEN}apiKey Set.${NC}"
+echo "apiKey set - $log_timestamp" >> /tmp/bb-tracker/log.txt
+}
+
+api_key_validator () {
+if [ -s ~/.config/bb-tracker/api-key.txt ]; then
+echo "apiKey detected - $log_timestamp" >> /tmp/bb-tracker/log.txt
+else
+echo -e "${RED}apiKey not detected..${NC}"
+echo "apiKey not detected - $log_timestamp" >> /tmp/bb-tracker/log.txt
+api_key_setup
+fi
+}
+
+api_key_validator
+
+username_validator () {
+/tmp/bb-tracker/calls/bb-tracker-login.sh
+if [ -s ~/.config/bb-tracker/username.txt ]; then
+echo "Username detected - $log_timestamp" >> /tmp/bb-tracker/log.txt
+else
+echo -e "${RED}Username not found..${NC}"
+echo "Username not detected - $log_timestamp" >> /tmp/bb-tracker/log.txt
+fi
+}
+
+username_validator
+username=$(cat ~/.config/bb-tracker/username.txt)
+echo -e "${YELLOW}Welcome, $username.${NC}"
